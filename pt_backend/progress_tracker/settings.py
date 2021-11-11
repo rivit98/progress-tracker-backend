efc671666e17c7f49ecfed9eb4ea74bb964db96f
Rivit <albert98g@gmail.com>
2021-11-11 11:16:02 +0100

    bump tokens lifetime

--- pt_backend/progress_tracker/settings.py
+++ pt_backend/progress_tracker/settings.py
@@ -113,8 +113,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True
     # 'BLACKLIST_AFTER_ROTATION': True
 }
