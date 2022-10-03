91fb14a38ff0b3f89606c2f23b24b88960cde9b6
Rivit <albert98g@gmail.com>
2022-10-03 20:51:02 +0200

    update workflows

--- lint.sh
+++ lint.sh
@@ -6,6 +6,3 @@ set -o errexit
 isort --check-only --diff .
 black --diff --check .
 flake8 --show-source .
-
-# Indents are four spaces, binary ops can start a line, and indent switch cases
-shfmt -i 4 -bn -ci -d .
