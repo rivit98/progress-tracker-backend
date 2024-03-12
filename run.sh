10664b997aca80f37c331c9dc0e64299d8d6a8b6
rivit98 <albert98g@gmail.com>
2024-03-12 20:40:55 +0100

    update

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@
 set -e
 
 python manage.py collectstatic --noinput
-python manage.py migrate
 python manage.py runscheduler &
 gunicorn progress_tracker.wsgi --bind 0.0.0.0:5001 --workers 3 --preload
