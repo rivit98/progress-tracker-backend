617b93b2f6656db6a5b572d464bceb060520550d
rivit98 <albert98g@gmail.com>
2024-11-30 22:39:36 +0100

    decrease workers num

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -e
 
 python manage.py collectstatic --noinput
 python manage.py runscheduler &
-gunicorn progress_tracker.wsgi --bind 0.0.0.0:5001 --workers 3 --preload
+gunicorn progress_tracker.wsgi --bind 0.0.0.0:5001 --workers 1 --preload
