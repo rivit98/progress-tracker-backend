f9e366df71bb21ad1768e37a67a16444f2219c50
rivit98 <albert98g@gmail.com>
2024-11-30 23:07:17 +0100

    fix format

--- progress_tracker/wsgi.py
+++ progress_tracker/wsgi.py
@@ -10,5 +10,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "progress_tracker.settings")
 application = get_wsgi_application()
